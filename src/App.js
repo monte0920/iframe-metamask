271ed6e34b415a550dfee91dfbddd6d16237f930
Josip0920 <eugene20920@gmail.com>
2022-10-11 07:39:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        title="iframe"
+        style={{ width: "100%", height: "100%" }}
+        src="https://www.zionlabs.info/"
+      ></iframe>
     </div>
   );
 }
